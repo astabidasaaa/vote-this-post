fe9ce07521b56b3e89bf3243f3fcc64455e4198b
astabidasaaa <astabidasabcd@gmail.com>
2024-02-27 00:33:27 +0700

    chg postBody input with textarea

--- app/(main)/home/PostInput.tsx
+++ app/(main)/home/PostInput.tsx
@@ -19,7 +19,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ReloadIcon } from "@radix-ui/react-icons";
+import { PlusCircledIcon, ReloadIcon, TrashIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 // import {
@@ -41,8 +42,16 @@ const formSchema = z.object({
   postBody: z
     .string()
     .min(1, { message: "Post must have content" })
-    .max(280, { message: "Post must be 280 or fewer characters long" }),
-  candidates: z.array(candidateSchema),
+    .max(280)
+    .regex(/\S.*$/),
+  // .max(280, { message: "Post must be 280 or fewer characters long" }),
+  candidates: z
+    .array(candidateSchema)
+    .nonempty({
+      message: "Can't be empty!",
+    })
+    .min(2, "must contain 2 or more items")
+    .max(6, "must contain 6 or fewer items"),
 });
 
 const PostInput = () => {
@@ -50,7 +59,7 @@ const PostInput = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       postBody: "",
-      candidates: [],
+      candidates: [{ candidateBody: "" }, { candidateBody: "" }],
     },
   });
 
@@ -68,9 +77,8 @@ const PostInput = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ postBody, candidates }),
+        body: JSON.stringify({ postBody: postBody.trim(), candidates }),
       });
-
       if (res.ok) {
         const test = await res.json();
         //   await signIn("credentials", {
@@ -79,7 +87,6 @@ const PostInput = () => {
         //     redirect: true,
         //     callbackUrl: "/home",
         //   });
-
         //     // await fetch("/api/auth/login", {
         //     //   method: "POST",
         //     //   headers: {
@@ -87,17 +94,40 @@ const PostInput = () => {
         //     //   },
         //     //   body: JSON.stringify({ username, password }),
         //     // });
+        form.reset();
+        // document.querySelector('div[data-inputid="postTextArea"]').innerText =
+        //   "";
         console.log(test);
       }
     } catch (err: any) {
       alert("Oops! Something went wrong. Please try again later");
     }
   };
+
+  const [content, setContent] = useState("");
+
+  const handleChange = (event: any) => {
+    setContent(event.target.innerText);
+    form.setValue("postBody", event.target.innerText.toString());
+    // console.log(event.target.innerText);
+  };
+
+  // useEffect(() => {
+  //   console.log(`content: ${content}`);
+  // }, [content]);
+
+  // useEffect(() => {
+  //   document.get;
+  // });
+
+  // =============== NOTES ===============
+  // ketika prompt value error (>280), tombol post tidak langsung disabled. cek div onInput
+
   return (
-    <Card className="w-[350px]">
+    <Card className="w-full max-w-xl">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        {/* <CardTitle>Create your prompt</CardTitle> */}
+        {/* <CardDescription>Post your new prompt in one-click</CardDescription> */}
       </CardHeader>
       <CardContent>
         <Form {...form}>
@@ -107,47 +137,135 @@ const PostInput = () => {
                 control={form.control}
                 name="postBody"
                 render={({ field }) => (
-                  <FormItem>
-                    {/* <FormLabel className="font-bold">Username</FormLabel> */}
-                    <FormControl>
-                      <Input placeholder="Your vote prompt" {...field} />
-                    </FormControl>
+                  <>
+                    <div className="relative w-full overflow-auto max-h-[240px] pb-2 mb-2 border-b-[1px]">
+                      {/* <div
+                        // {...form.register("postBody")}
+                        // onChange={(e) =>
+                        //   console.log(
+                        //     form.setValue("postBody", e.target.innerText)
+                        //   )
+                        // }
+                        // onInput={(e: any) => console.log(field)}
+                        onInput={(e: any) => {
+                          // form.setValue(
+                          //   "postBody",
+                          //   e.target.innerText.toString(),
+                          //   { shouldValidate: true }
+                          // );
 
-                    <FormMessage />
-                  </FormItem>
+                          handleChange(e);
+
+                          // console.log(field.value);
+                        }}
+                        // onBlur={() => field.onBlur()}
+                        // onBlur={() => console.log("blur")}
+                        // onChange={() => console.log("change")}
+                        // onFocus={() => console.log("focus")}
+                        // onInput={(e) => handleChange(e)}
+                        tabIndex={0}
+                        contentEditable="plaintext-only"
+                        role="textbox"
+                        // {...field}
+                        onChange={field.onChange}
+                        onBlur={field.onBlur}
+                        ref={field.ref}
+                        data-inputid="postTextArea"
+                        className="focus:outline-none select-text whitespace-pre-wrap break-words"
+                      >
+                        {/* <span>{form.getValues("postBody")}</span> */}
+                      {/* <span>{field.value}</span> */}
+                      {/* {form.getValues("postBody")} */}
+                      {/* </div> */}
+                      {field.value.length <= 0 && (
+                        <span className="text-muted-foreground absolute top-0 pointer-events-none select-none">
+                          Ask your question
+                        </span>
+                      )}
+                      <textarea
+                        {...field}
+                        className="w-full resize-none focus:outline-none select-text whitespace-pre-wrap break-words min-h-[72px] max-h-[200px] pr-1"
+                      ></textarea>
+                      {/* <Input
+                        type="hidden"
+                        placeholder="Your question"
+                        {...field}
+                        // value={content}
+                      /> */}
+                      <FormDescription
+                        className={`${
+                          field.value.length <= 280
+                            ? "text-muted-foreground"
+                            : "text-destructive"
+                        } text-xs text-end`}
+                      >
+                        {280 - field.value.length || "0"}
+                      </FormDescription>
+                    </div>
+
+                    {/* <FormMessage /> */}
+                  </>
                 )}
               />
-              <ul>
+
+              <ul className="flex flex-col justify-start items-start gap-4">
                 {fields.map((item, index) => {
                   return (
-                    <li key={item.id}>
+                    <li key={item.id} className="w-full flex flex-col gap-1">
                       <FormField
                         control={form.control}
                         name={`candidates.${index}.candidateBody`}
                         render={({ field }) => (
-                          <FormItem>
-                            {/* <FormLabel className="font-bold">Username</FormLabel> */}
-                            <FormControl>
-                              <Input placeholder="Your candidate" {...field} />
-                            </FormControl>
+                          <>
+                            <div className="flex flex-row w-full justify-end items-start gap-2">
+                              <div className="relative flex flex-col w-full gap-1">
+                                <FormItem className="relative flex w-full items-center justify-center space-x-2">
+                                  <FormControl>
+                                    <Input
+                                      autoComplete="off"
+                                      placeholder={`Choice ${index + 1}`}
+                                      maxLength={32}
+                                      {...field}
+                                    />
+                                  </FormControl>
+                                </FormItem>
+                                <FormDescription className="absolute bottom-1 right-2 self-end text-end text-xs">
+                                  {field.value.length} / 32
+                                </FormDescription>
+                                {/* <div className="flex flex-row justify-end items-center w-full">
+                                  <FormMessage />
+                                  <FormDescription className="self-end text-end text-xs">
+                                    {field.value.length} / 32
+                                  </FormDescription>
+                                </div> */}
+                              </div>
+                              <Button
+                                variant="ghost"
+                                size="icon"
+                                disabled={fields.length <= 2}
+                                onClick={() => remove(index)}
+                              >
+                                <TrashIcon />
+                              </Button>
+                            </div>
 
-                            <FormMessage />
-                          </FormItem>
+                            {/* <FormMessage /> */}
+                          </>
                         )}
                       />
-                      <button type="button" onClick={() => remove(index)}>
-                        Delete
-                      </button>
                     </li>
                   );
                 })}
               </ul>
-              <button
+              <Button
                 type="button"
+                variant="ghost"
                 onClick={() => append({ candidateBody: "" })}
+                disabled={fields.length >= 6}
               >
-                append
-              </button>
+                <PlusCircledIcon className="mr-2" />
+                add
+              </Button>
               {/* <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Name</Label>
                 <Input id="name" placeholder="Name of your project" />
@@ -157,15 +275,32 @@ const PostInput = () => {
                 <Input id="name" placeholder="Your candidate" />
               </div> */}
 
-              <CardFooter className="flex justify-between">
-                <Button variant="outline">Cancel</Button>
-                <Button type="submit" disabled={form.formState.isSubmitting}>
+              <CardFooter className="flex justify-end p-0">
+                {/* <Button variant="outline">Cancel</Button> */}
+                <Button
+                  type="submit"
+                  disabled={
+                    !form.formState.isValid || form.formState.isSubmitting
+                  }
+                >
                   {form.formState.isSubmitting ? (
                     <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
                   ) : (
                     "Post"
                   )}
                 </Button>
+                {/* <Button
+                  type="button"
+                  onClick={() => console.log(form.getValues("postBody"))}
+                >
+                  test
+                </Button>
+                <Button
+                  type="button"
+                  onClick={() => console.log(form.formState.isValid)}
+                >
+                  valid?
+                </Button> */}
               </CardFooter>
             </div>
           </form>
@@ -176,3 +311,42 @@ const PostInput = () => {
 };
 
 export default PostInput;
+
+// {/* <FormField
+//   control={form.control}
+//   name="postBody"
+//   render={({ field }) => (
+//     <FormItem>
+//       {/* <FormLabel className="font-bold">Username</FormLabel> */}
+//       <FormControl>
+//         <div
+//           className="w-full min-h-8 max-h-[720px] text-xl break-words focus:outline-none focus:border-none"
+//           contentEditable="true"
+//           {...field}
+//           placeholder="test"
+//           onInput={handleChange}
+//           onChange={() => field.onChange()}
+//           // {...form.register("postBody")}
+//           // dangerouslySetInnerHTML={{ __html: content }}
+//         >
+//           {/* <span
+//                           className="w-full"
+//                           // contentEditable="true"
+//                           placeholder="test"
+//                           // {...field}
+//                         ></span> */}
+//           {/* <span></span> */}
+//         </div>
+//         {/* <Input
+//                         className="text-pretty overflow h-auto max-h-64 break-words"
+//                         placeholder="Your question"
+//                         {...field}
+//                       /> */}
+//       </FormControl>
+//       <FormLabel className=" text-xs" onClick={() => console.log(field)}>
+//         {280 - field.value.length}
+//       </FormLabel>
+//       {/* <FormMessage /> */}
+//     </FormItem>
+//   )}
+// />; */}
--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -4,11 +4,14 @@
 import React, { useState, useEffect } from "react";
 // import connectDB from "../lib/mongoose";
 // import VoteModel from "../models/Vote";
-import SignInBtn from "@/components/SignInBtn";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useLayoutEffect } from "react";
-import { redirect } from "next/navigation";
-import LoadingPage from "@/components/Loading";
+// import SignInBtn from "@/components/SignInBtn";
+// import { signIn, signOut, useSession } from "next-auth/react";
+// import { useLayoutEffect } from "react";
+// import { redirect } from "next/navigation";
+// import LoadingPage from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+import PostInput from "./PostInput";
+import Timeline from "./Timeline";
 
 interface Candidates {
   _id: string;
@@ -19,11 +22,7 @@ interface Candidates {
   rating: string;
 }
 
-const VotingPage: React.FC = () => {
-  const [candidates, setCandidates] = useState<Candidates[]>([]);
-
-  const [votePrompts, setVotePrompts] = useState([]);
-
+const HomePage = () => {
   // useEffect(() => {
   //   const fetchVotePrompts = async () => {
   //     const response = await fetch("/api/vote", {
@@ -36,9 +35,6 @@ const VotingPage: React.FC = () => {
   //   fetchVotePrompts();
   // }, []);
 
-  useEffect(() => {
-    console.log(votePrompts);
-  }, [votePrompts]);
   // useEffect(() => {
   //   // Fetch candidates from MongoDB
   //   const fetchData = async () => {
@@ -66,25 +62,62 @@ const VotingPage: React.FC = () => {
   //   }
   // };
 
-  const { status } = useSession();
+  // const { data, status } = useSession({
+  //   required: true,
+  //   onUnauthenticated() {
+  //     redirect("/login");
+  //     // The user is not authenticated, handle it here.
+  //   },
+  // });
 
   // useLayoutEffect(() => {
   // const isAuth = status;
 
-  // console.log(status);
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
-  if (status === "unauthenticated") {
-    redirect("/login");
-  }
-  // }, [status]);
+  const handleTestJWT = async () => {
+    try {
+      const res = await fetch("/api/test-api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify({ username: "aaa", password: "1234" }),
+      });
 
-  if (status === "loading") return <LoadingPage />;
+      if (res.ok) {
+        console.log(await res.json());
+      }
+    } catch (err: any) {
+      alert("Oops! Something went wrong. Please try again later");
+    }
+  };
+
+  // const handleTestSession = async () => {
+  //   try {
+  //     console.log(data);
+  //   } catch (err: any) {
+  //     alert("Oops! Something went wrong. Please try again later");
+  //   }
+  // };
+
+  // if (status === "unauthenticated") {
+  //   redirect("/login");
+  // }
+
+  // if (status === "loading") return <LoadingPage />;
 
   return (
-    <div>
-      <h2>Hello world</h2>
-      <SignInBtn />
-
+    <div className="">
+      {/* <h2>Hello world</h2>
+      <Button onClick={handleTestJWT}>JWT</Button> */}
+      <div className="w-full py-8 px-4 md:px-12 lg:px-24 flex flex-col justify-start items-center gap-12">
+        <PostInput />
+        <Timeline />
+      </div>
+      {/* <Button onClick={handleTestSession}>Session</Button> */}
       {/* {candidates.map((candidate) => (
         <div key={candidate._id}>
           <h3>{candidate.candidate}</h3>
@@ -96,4 +129,4 @@ const VotingPage: React.FC = () => {
   );
 };
 
-export default VotingPage;
+export default HomePage;
